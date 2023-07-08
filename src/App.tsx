2f8cd678355a8456b7e204c71def7daa95b8ae1e
Rua427 <ekrua427@naver.com>
2023-07-08 18:17:04 +0900

    webpack resource processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './styles.css'
 import IMAGE from './react.png'
+import LOGO from './logo.svg'
 
 const App = () => {
   return (
     <div>
         <h1>App</h1>
         <img src={IMAGE} alt="React Logo"  width="320" height="180"/>
+        <img src={LOGO} alt="React Logo"  width="300"/>
     </div>
   )
 }
