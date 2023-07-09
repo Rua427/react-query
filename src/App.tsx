d29fcea6d7c0c7bad56c979adbfee683c4d4cdd1
Rua427 <ekrua427@naver.com>
2023-07-09 17:27:12 +0900

    setuped webpack

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import LOGO from './logo.svg'
 import ClickCounter from './ClickCounter'
 
 const App = () => {
+  const name = 'Vishwas'
   return (
     <div>
-        <h1>eidted - {process.env.NODE_ENV} {process.env.name}</h1>
-        <img src={IMAGE} alt="React Logo"  width="320" height="180"/>
-        <img src={LOGO} alt="React Logo"  width="300"/>
-        <ClickCounter/>
+      <h1>
+        eidted - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="320" height="180" />
+      <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCounter />
     </div>
   )
 }
