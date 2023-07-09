f4131dcd9f786f999680c32e28fdfacb64e0c3a8
Rua427 <ekrua427@naver.com>
2023-07-09 16:00:02 +0900

    intalling the refresh protection packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
+import ClickCounter from './ClickCounter'
 
 const App = () => {
   return (
     <div>
-        <h1>App - {process.env.NODE_ENV} {process.env.name}</h1>
+        <h1>eidted - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo"  width="320" height="180"/>
         <img src={LOGO} alt="React Logo"  width="300"/>
+        <ClickCounter/>
     </div>
   )
 }
